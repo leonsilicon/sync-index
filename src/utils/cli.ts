dd20a461fb132b30ed1b3f94a93318f66c35a1dd
Leon Si <contact@leonzalion.com>
2022-01-13 14:47:40 -0500

    changes

--- src/utils/cli.ts
+++ src/utils/cli.ts
@@ -24,7 +24,7 @@ export async function syncIndexCli() {
 	program.parse();
 
 	const configOptions = await getConfigOptions();
-	 
+
 	const options: SyncIndexOptions = {
 		...configOptions,
 		...program.opts<SyncIndexOptions>(),
