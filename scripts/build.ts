8e660510593230480dcfeada441475d2656b6f04
Leon Si <contact@leonzalion.com>
2022-01-13 17:50:47 -0500

    fix paths with tsc alias

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,5 +3,6 @@ import * as fs from 'node:fs';
 import { execaCommandSync as exec } from 'execa';
 
 exec('tsc');
+exec('tsc-alias -p tsconfig.json');
 for (const file of ['readme.md', 'package.json'])
 	fs.copyFileSync(file, path.join('dist', file));
