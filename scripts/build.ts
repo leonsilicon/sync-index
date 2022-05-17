5b8c92caa983ac597e932d02c4ebdd2f856fa334
Leon Si <contact@leonzalion.com>
2022-05-17 19:19:45 -0400

    refactor: remove lion-system

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,5 @@
 import { execaCommandSync as exec } from 'execa';
-import { chProjectDir, rmDist, copyPackageFiles } from 'lion-system';
+import { chProjectDir, rmDist, copyPackageFiles } from 'lionconfig';
 
 chProjectDir(import.meta.url);
 rmDist();
